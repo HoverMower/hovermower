b0c5320e64c5b3410ad334146f2ac3c013527acb
PaddyCube <paddy-daun@web.de>
2024-01-10 19:19:21 +0000

    rtk gps

--- hovermower_bringup/launch/gps.launch.py
+++ hovermower_bringup/launch/gps.launch.py
@@ -3,7 +3,11 @@ import os
 import ament_index_python.packages
 import launch
 import launch_ros.actions
-
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     config_directory = os.path.join(
@@ -15,8 +19,20 @@ def generate_launch_description():
                                              output='both',
                                              parameters=[params])
 
-    return launch.LaunchDescription([ublox_gps_node,
+    params_ntrip_client = os.path.join(config_directory, 'ntrip_client.yaml')
+    ntrip_client = Node(
+                package='ntrip_client',
+                executable='ntrip_ros.py',
+                parameters=[params_ntrip_client],
+                # Uncomment the following section and replace "/gx5/nmea/sentence" with the topic you are sending NMEA on if it is not the one we requested
+                #remappings=[
+                #  ("nmea", "/gx5/nmea/sentence")
+                #],
+          )
 
+    
+    return launch.LaunchDescription([ublox_gps_node,
+                                     ntrip_client,
                                      launch.actions.RegisterEventHandler(
                                          event_handler=launch.event_handlers.OnProcessExit(
                                              target_action=ublox_gps_node,
--- hovermower_bringup/readme.md
+++ hovermower_bringup/readme.md
@@ -6,3 +6,20 @@ To start a real Hovermower use this command
 ```
 ros2 launch hovermower_bringup teleop.launch.py
 ```
+
+# Prerequisites
+## ublox driver
+You need to install ublox driver from here [KumarRobotics ublox](https://github.com/KumarRobotics/ublox). Beware to use the ros2 branch.
+This package also requires nmea-msgs package, which you may also need to install manually for succesful build.
+
+Please don't install the package by 
+```
+sudo apt install ros-$ROS_DISTRO-ublox*
+```
+this may install the wrong version. I tried it with this first but I noted that this will not subscribe to /rtcm topic, which is needed to use corretion data of your base station or public ntrip server.
+
+## ntrip client
+To send RTCM correction data to ublox driver (to /rtcm topic), you need a ntrip client, which connects to your ntrip serve r(i.e. your own base station). To achieve this, I use this ntrip client, which sends data as ros topic [ntrip client](https://github.com/LORD-MicroStrain/ntrip_client/).
+
+This solution has been provided by [olvdhrm](https://github.com/olvdhrm/RTK_GPS_NTRIP). But he had to add a message translation as both packages uses different message types. The ntrip-client repository provides a branch named feature/ros2_prefer_rtcm_msgs, which seems to fix this. At time of this writing (Janary 2024), it was necessary to use the feature branch.
+
